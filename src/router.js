39cccd66cb7d25a801ceca34eac24a27f611f13c
Douglas Ray <dpr980@gmail.com>
2019-05-05 16:21:35 +0100

    create user with existing username returns an error

--- src/router.js
+++ src/router.js
@@ -1,24 +1,28 @@
 import express from 'express'
+import moment from 'moment'
 
 import { Exercise, User } from './models'
-import { runInNewContext } from 'vm'
 
 const Router = express.Router()
 
-// I can create a user by posting form data username to /api/exercise/new-user and returned will be an object with username and \_id.
+const isValidDate = date => moment(date).isValid()
+
+// I can create a user by posting form data username to /api/exercise/new-user
+// and an object with username and id will be returned.
 Router.post('/new-user', (req, res, next) => {
 	const { username } = req.body
 	// create new user
 	const user = new User({ username })
 	user.save((err, data) => {
-		console.log(data, 'Person saved')
-		// send username and id in response to client
-		return res.json({ username: data.username, _id: data._id })
+		// does username exist in db already?
+		err && err.code === 11000
+			? res.json('Error: That username is taken. Try a different one.')
+			: res.json({ username: data.username, _id: data._id })
 	})
-	// does username exist in db already?
 })
 
-// I can get an array of all users by getting api/exercise/users with the same info as when creating a user.
+// I can get an array of all users by getting api/exercise/users
+// with the same info as when creating a user.
 Router.get('/users', (req, res, next) => {
 	User.find()
 		.select('username _id')
@@ -28,19 +32,27 @@ Router.get('/users', (req, res, next) => {
 		})
 })
 
-// I can add an exercise to any user by posting form data userId(\_id), description, duration, and optionally date to /api/exercise/add.
-// If no date supplied it will use current date. Returned will the the user object with the exercise fields added.
+// I can add an exercise to any user by posting form data containing the userId, description, duration,
+// and date (optional) to /api/exercise/add. If no date supplied it will use current date.
+// Return the the user object with the exercise fields added.
 Router.post('/add', (req, res, next) => {
 	const { userId, description, duration } = req.body
-	const exercise = new Exercise({ userId, description, duration })
-	const date = req.body.date || new Date()
+	const date = req.body.date ? new Date(req.body.date) : new Date()
 
 	if (!userId || !description || !duration) {
 		return console.log('empty fields')
 	}
 
+	let user
+	User.findById(userId)
+		.select('username _id')
+		.exec((err, data) => {
+			err ? console.log(err) : (user = data)
+		})
+	const exercise = new Exercise({ userId, description, duration, date })
+
 	exercise.save((err, data) => {
-		console.log(data)
+		console.log({ ...data, ...user })
 	})
 })
 
